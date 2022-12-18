a93e8530ebbdc05f401ef89ffa2a3d60263e0c4b
pranav kamera <99005620+pranavkps@users.noreply.github.com>
2022-12-18 09:49:14 +0530

    Update finalscore.js

--- src/components/finalscore.js
+++ src/components/finalscore.js
@@ -1,10 +1,10 @@
 import React from "react";
 import './login.css'
 
-const FinalScore = () =>{
+const FinalScore = ({score1}) =>{
     return(
         <div>
-            <h1>FinalScore </h1>
+            <h1>FinalScore of your Quiz is {score1} </h1>
         </div>
     )
 }
